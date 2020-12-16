576e5e696027b3a18a5eca8734363f4f90bf7565
HuffDaddy0 <ty.huff24@gmail.com>
2020-12-15 21:29:31 -0500

    began building controllers

--- modeling-backend/app/controllers/albums_controller.rb
+++ modeling-backend/app/controllers/albums_controller.rb
@@ -1,2 +1,11 @@
 class AlbumsController < ApplicationController
+
+    def index
+        albums = Album.all
+    
+        render json: albums, include: :photos
+    end
+
+
+
 end
--- modeling-backend/app/controllers/application_controller.rb
+++ modeling-backend/app/controllers/application_controller.rb
@@ -1,2 +1,3 @@
 class ApplicationController < ActionController::Base
+    enable_session :true
 end
